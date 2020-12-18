607eca48b3d756ab02eeb26838701a1fa70dab5b
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-12-19 00:15:00 +0530

    Create array_previous_less.js

---
+++ array_previous_less.js
@@ -0,0 +1,42 @@
+/**
+Given an array of integers, for each position i, search among the previous positions for the last (from the left) position
+that contains a smaller value. Store that value at position i in the answer. If no such value can be found, store -1 instead.
+
+Example
+For items = [3, 5, 2, 4, 5], the output sholuld be [-1, 3, -1, 2, 4]
+
+Hints
+unshift()
+**/
+
+function arrayPreviousLess(nums) {
+    let arrPrevLess = [-1];
+    for(let i=0; i < nums.length-1; i++) {
+        if(nums[i+1] > nums[i]){
+            arrPrevLess.push(nums[i])
+        }
+        else {
+            arrPrevLess.push(-1);
+        }
+    }
+    return arrPrevLess;
+}
+
+/**
+* Test Suite 
+*/
+describe('arrayPreviousLess()', () => {
+    it('shift previous postions from the left to a smaller value or store -1', () => {
+        // arrange
+        const nums = [3, 5, 2, 4, 5];
+        
+        // act
+        const result = arrayPreviousLess(nums);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([-1, 3, -1, 2, 4]);
+    });
+});
