0651d33f7c43defea34f49f152f6526944345c8b
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-12-19 12:42:48 +0530

    Update array_previous_less.js

--- array_previous_less.js
+++ array_previous_less.js
@@ -23,6 +23,26 @@ function arrayPreviousLess(nums) {
 }
 
 /**
+// Another Way
+function arrayPreviousLess(nums) {
+    const previousLess = [];
+    
+    for(let i = nums.length -1; i >= 0; i--) {
+        for(let j = i; j >= 0; j--) {
+            if(nums[i] > nums[j]) {
+                previousLess.unshift(nums[j]);
+                break;
+            } else if (j === 0) {
+                previousLess.unshift(-1);
+            }
+        }
+    }
+    
+    return previousLess;
+}
+/**
+
+/**
 * Test Suite 
 */
 describe('arrayPreviousLess()', () => {
