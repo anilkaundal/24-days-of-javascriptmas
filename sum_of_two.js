1749ee6d3f0c0aa88dca6d08374b835b95b6cf86
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-12-22 21:06:15 +0530

    Update sum_of_two.js

--- sum_of_two.js
+++ sum_of_two.js
@@ -24,6 +24,26 @@ function sumOfTwo(nums1, nums2, value) {
 }
 
 /**
+// Another Way
+function sumOfTwo(nums1, nums2, value) {
+    const map = {};
+    
+    for(let num of nums1) {
+        const difference = value - num;
+        map[difference] = difference;
+    }
+    
+    for(let num of nums2) {
+        if(map.hasOwnProperty(num)) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+**/
+
+/**
 * Test Suite 
 */
 describe('sumOfTwo()', () => {
